2b44609690704780911efe822bdee29176c99a3c
李宸乐 <lichenle@lichenledeMacBook-Pro.local>
2017-04-01 16:47:55 +0800

    3 methods added

--- src/feature_cluster.py
+++ src/feature_cluster.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 
 from numpy import *
-from sklearn import cluster, datasets
+from sklearn import cluster, datasets, metrics
 
 """Load raw data set"""
 def loadDataSet(filename):
@@ -30,3 +30,63 @@ def main():
     print(cluster_centers)
     print("Sum of distance")
     print(cluster_inertia)
+
+"""
+Birch http://www.cnblogs.com/pinard/p/6200579.html
+"""
+def BirchCluster(dataset):
+    birch = cluster.Birch(n_clusters=3)
+    birch.fit(data)
+    return birch.subclusters_centers_, birch.labels
+
+def main():
+    data = loadDataSet('data/data.cv')
+    cluster_centers, cluster_labels = birch(data)
+    Calinski_Harabasz = metrics.calinski_harabaz_score(data, birch.fit(data))
+    print('Cluster_centers:')
+    print(cluster_centers)
+    print('Cluster_labels')
+    print(cluster_labels)
+    print('Calinski_Harabasz Score')
+    print(Calinski_Harabasz)
+
+"""
+DBSCAN http://www.cnblogs.com/pinard/p/6208966.html
+"""
+def DBSCANCluster(dataset):
+    dbscan = cluster.dbscan()
+    dbscan.fit(data)
+    return dbscan.labels_
+
+def main():
+    data = loadDataSet('data/data.csv')
+    cluster_labels = DBSCANCluster(data)
+    n_clusters = len(set(cluster_labels)) - (1 if -1 in labels else 0)
+    completeness = metrics.completeness_score(cluster_labels_true, cluster_labels) #The percentage of data rest
+    Calinski_Harabasz = metrics.calinski_harabaz_score(data, dbscan.fit(data))
+    print('n_clusters;')
+    print(n_clusters)
+    print('Completeness')
+    print(completeness)
+    print('Calinski_Harabasz')
+    print(Calinski_Harabasz)
+
+"""
+Hierarchical clustering
+"""
+def HClustering(dataset):
+    hclustering = cluster.AgglomerativeClustering(n_clusters=3)
+    hclustering.fit()
+    return hclustering.labels_
+
+def main():
+    data = loadDataSet('data/data.csv')
+    cluster_labels = HClustering(data)
+    n_clusters = len(set(cluster_labels)) - (1 if -1 in labels else 0)
+    Calinski_Harabasz = metrics.calinski_harabaz_score(data, dbscan.fit(data))
+    print('n_clusters;')
+    print(n_clusters)
+    print('Calinski_Harabasz')
+    print(Calinski_Harabasz)
+
+
